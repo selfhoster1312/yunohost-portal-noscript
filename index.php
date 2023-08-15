ebd2e807a070f27b7826ba038e3b3dd02b937197
selfhoster1312 <selfhoster1312@kl.netlib.re>
2023-08-15 17:39:44 +0200

    Rework login page

--- index.php
+++ index.php
@@ -1,40 +1,26 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Yunohost SSO</title>
-  </head>
-  <body>
 <?php
-    if (array_key_exists("REMOTE_USER", $_SERVER) && $_SERVER["REMOTE_USER"] != "") {
-        echo "Welcome, " . $_SERVER["REMOTE_USER"] . "!";
-        echo "<br><a href='/yunohost/portalapi/logout?referer_redirect'>Log out</a><br>";
-    } else {
+	require_once 'vendor/autoload.php';
+	$loader = new \Twig\Loader\FilesystemLoader('templates');
+	$twig = new \Twig\Environment($loader, [
+	    //'cache' => 'compilation_cache',
+	]);
+
+	$env = [];
+	$logged_in = false;
+	if (array_key_exists("REMOTE_USER", $_SERVER) && $_SERVER["REMOTE_USER"] != "")  {
+		$logged_in = true;
+		$env["user"] = $_SERVER["REMOTE_USER"];
+		$headers = getallheaders();
+		if (array_key_exists("Remote-User", $headers)) {
+			$env["http_user"] = $headers["Remote-User"];
+		}
+	}
+	
+	if ($logged_in == true) {
+		$env["headers"] = getallheaders();
+		echo $twig->render("panel.html.twig", $env);
+	}
+	else {
+		echo $twig->render("login.html.twig", []);
+	}
 ?>
-    <form method="POST" action="/yunohost/portalapi/login?referer_redirect">
-        <input type="text" name="username" id="username">
-        <br><input type="password" name="password" id="password">
-        <br><input type="submit">
-    </form>
-<?php
-    }
-?>
-    <br><br>
-<?php
-    $headers = getallheaders();
-    if (array_key_exists("Remote-User", $headers)) {
-        if ($_SERVER["REMOTE_USER"] != $headers["Remote-User"]) {
-            echo "FastCGI REMOTE_USER and HTTP Remote-User header do not match: " . $_SERVER["REMOTE_USER"] . " vs " . $headers["Remote-User"];
-        } else {
-            echo "FastCGI REMOTE_USER and HTTP Remote-User header match.";
-        }
-    } else {
-        if (array_key_exists("REMOTE_USER", $_SERVER) && $_SERVER["REMOTE_USER"] != "") {
-                echo "Missing HTTP header: Remote-User";
-                foreach($headers as $key => $val) {
-                        echo "<br>$key: $val";
-                }
-        }
-    }
-?>
-  </body>
-</html>
